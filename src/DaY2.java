4ee913900af0fb88fec3fc7db16bd4ae428ca907
abbassarisg <42302901+abbassarisg@users.noreply.github.com>
2022-08-15 22:24:49 +0300

    Update DaY2.java
    Github Pull

--- src/DaY2.java
+++ src/DaY2.java
@@ -1,5 +1,8 @@
 public class DaY2 {
     public static void main(String[] args) {
         System.out.println("deneme");
+        System.out.println("Github tan pull olan");
+        
+          
     }
 }
